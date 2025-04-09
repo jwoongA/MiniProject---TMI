1916b94f4a96720a2de1eedbc4e336679861084f
Youngim <immy0815@users.noreply.github.com>
2025-04-09 11:14:59 +0900

    board 스크립트 정리

--- UnityProject/MiniProject_TMI/Assets/Scripts/Board.cs
+++ UnityProject/MiniProject_TMI/Assets/Scripts/Board.cs
@@ -10,72 +10,84 @@ public class Board : MonoBehaviour
     {
         int difficulty = PlayerPrefs.GetInt("SelectedDifficulty");
 
-        if (GameManager.Instance.curStage == 1)
+        int curStageNum = GameManager.Instance.curStage;
+
+        switch (curStageNum)
         {
-            // 스테이지 1 배치
-            int[] arr = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4 };
-            arr = arr.OrderBy(x => Random.Range(0f, 4f)).ToArray();
+            case 1:
+                Stage1();
+                break;
+            case 2:
+                Stage2();
+                break;
+        }
+    }
 
-            float spacing = 1.5f;
+    void Stage1()
+    {
+        // 스테이지 1 배치
+        int[] arr = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4 };
+        arr = arr.OrderBy(x => Random.Range(0f, 4f)).ToArray();
 
-            // 상단 3x2
-            for (int i = 0; i < 6; i++)
-            {
-                GameObject go = Instantiate(this.card, this.transform);
+        float spacing = 1.5f;
 
-                int column = i % 3; // 0 1 2
-                int row = i / 3;
+        // 상단 3x2
+        for (int i = 0; i < 6; i++)
+        {
+            GameObject go = Instantiate(this.card, this.transform);
 
-                float x = (column - 1) * spacing;  // -1, 0, 1
-                float y = (1 - row) * spacing;     // 1, 0
+            int column = i % 3; // 0 1 2
+            int row = i / 3;
 
-                go.transform.position = new Vector2(x, y);
-                go.GetComponent<Card>().Setting(arr[i]);
-            }
+            float x = (column - 1) * spacing;  // -1, 0, 1
+            float y = (1 - row) * spacing;     // 1, 0
 
-            // 하단 2x2
-            for (int i = 0; i < 4; i++)
-            {
-                GameObject go = Instantiate(this.card, this.transform);
-
-                int column = (i % 2 == 0) ? -1 : 1;
-                int row = i / 2;
-
-                float x = column * (spacing / 2);  // - spacing / 2, spacing / 2
-                float y = (-1 - row) * spacing;      // -1, -2
-
-                go.transform.position = new Vector2(x, y);
-                go.GetComponent<Card>().Setting(arr[i + 6]);
-            }
-
-            GameManager.Instance.cardCount = arr.Length;
+            go.transform.position = new Vector2(x, y);
+            go.GetComponent<Card>().Setting(arr[i]);
         }
 
-        else if (GameManager.Instance.curStage == 2)
+        // 하단 2x2
+        for (int i = 0; i < 4; i++)
         {
-            int[] arr = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9 };
-            arr = arr.OrderBy(x => Random.Range(0f, 9f)).ToArray();
+            GameObject go = Instantiate(this.card, this.transform);
 
-            float spacing = 1.4f; // 카드 간격 (원하는 만큼 조절)
-            int columns = 4;
-            int rows = 5;
+            int column = (i % 2 == 0) ? -1 : 1;
+            int row = i / 2;
 
-            for (int i = 0; i < arr.Length; i++)
-            {
-                GameObject go = Instantiate(this.card, this.transform);
+            float x = column * (spacing / 2);  // - spacing / 2, spacing / 2
+            float y = (-1 - row) * spacing;      // -1, -2
 
-                int column = i % columns;
-                int row = i / columns;
-                float yOffset = -1.0f;
-
-                float x = (column - (columns - 1) / 2f) * spacing;
-                float y = ((rows - 1) / 2f - row) * spacing + yOffset; // 중앙 배치를 하게되면 카드가 시간을 가려서 전체적인 y 좌표를 아래로 내림.
-
-                go.transform.position = new Vector2(x, y);
-                go.GetComponent<Card>().Setting(arr[i]);
-            }
-
-            GameManager.Instance.cardCount = arr.Length;
+            go.transform.position = new Vector2(x, y);
+            go.GetComponent<Card>().Setting(arr[i + 6]);
         }
+
+        GameManager.Instance.cardCount = arr.Length;
+    }
+
+    void Stage2()
+    {
+        int[] arr = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9 };
+        arr = arr.OrderBy(x => Random.Range(0f, 9f)).ToArray();
+
+        float spacing = 1.4f; // 카드 간격 (원하는 만큼 조절)
+        int columns = 4;
+        int rows = 5;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            GameObject go = Instantiate(this.card, this.transform);
+
+            int column = i % columns;
+            int row = i / columns;
+            float yOffset = -1.0f;
+
+            float x = (column - (columns - 1) / 2f) * spacing;
+            float y = ((rows - 1) / 2f - row) * spacing + yOffset; // 중앙 배치를 하게되면 카드가 시간을 가려서 전체적인 y 좌표를 아래로 내림.
+
+            go.transform.position = new Vector2(x, y);
+            go.GetComponent<Card>().Setting(arr[i]);
+        }
+
+        GameManager.Instance.cardCount = arr.Length;
     }
 }
