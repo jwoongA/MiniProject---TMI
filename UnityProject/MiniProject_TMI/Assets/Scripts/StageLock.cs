5066a12ec5d7142a3f9b66e6f0ea58a18c606f00
halonine9 <dlselhvxg@gmail.com>
2025-04-09 16:36:58 +0900

    오류 수정전

--- UnityProject/MiniProject_TMI/Assets/Scripts/StageLock.cs
+++ UnityProject/MiniProject_TMI/Assets/Scripts/StageLock.cs
@@ -9,7 +9,9 @@ public class StageLock : MonoBehaviour
     [SerializeField] Button btnStage;
     [SerializeField] Shadow btnStageShadow;
     [SerializeField] int stageNum;
+    [SerializeField] Animator anim;
 
+   
     void Lock()
     {
         // Lock 오브젝트 활성화, 버튼 기능 비활성화(버튼 누름 방지), Shadow 비활성화(가시성)
@@ -22,7 +24,7 @@ public class StageLock : MonoBehaviour
     {
         // 해당 스크립트를 갖고 있는 Stage가 클리어 된 적있는지 체크
         // 정보가 없다면 Lock
-        bool stageClear = PlayerPrefs.GetInt($"Stage{this.stageNum}_Clear", 0) == 1;
+        bool stageClear = PlayerPrefs.GetInt($"Stage{this.stageNum-1}_Clear", 0) == 1;
         if (GameManager.Instance != null) // null 체크
         {
             if(GameManager.Instance.curStage + 1 == this.stageNum)
@@ -36,14 +38,18 @@ public class StageLock : MonoBehaviour
             Lock();
         }
     }
-
     public void UnLock()
     {
         // 여기에서 애니메이션 플레이하시면 됩니다.
-
+        anim.SetBool("isUnLock", true);
+        Invoke("InvokeUnLock", 2f);
+    }
+    public void InvokeUnLock()
+    {
         // Lock 오브젝트 비활성화, 버튼 기능 활성화, Shadow 활성화(가시성)
         this.stageLock.SetActive(false);
         this.btnStage.enabled = true;
         this.btnStageShadow.enabled = true;
-    }
+        anim.SetBool("isUnLock", false);
+    }   
 }
